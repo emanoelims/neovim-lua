4780805da5235e381b8a3f21b8756a28bcc62695
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-21 17:58:05 -0800

    feat: 2 spaces for selected filetypes

--- init.lua
+++ init.lua
@@ -30,3 +30,6 @@ opt.smartindent = true
 cmd [[au BufWritePre * :%s/\s\+$//e]]
 cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
 cmd [[autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0]]
+cmd [[
+  autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml setlocal shiftwidth=2 tabstop=2
+]]
