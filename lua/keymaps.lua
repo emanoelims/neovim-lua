7859212d4d2b75d0e9b05c3ece6b610744eb46a6
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-22 07:00:24 -0800

    feat: fast saving with <leader> and s

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,6 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 map('n', '<leader>c', ':nohl<CR>', opts)
+
+map('n', '<leader>s', ':w<CR>', opts)
+map('i', '<leader>s', '<C-c>:w<CR>', opts)
