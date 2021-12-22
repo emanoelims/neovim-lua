bdd5fc0848f916440c9f34f846713e0dd955504b
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-22 07:03:11 -0800

    feat: move around splits using Ctrl + {h,j,k,l}

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,3 +6,8 @@ map('n', '<leader>c', ':nohl<CR>', opts)
 
 map('n', '<leader>s', ':w<CR>', opts)
 map('i', '<leader>s', '<C-c>:w<CR>', opts)
+
+map('n', '<C-h>', '<C-w>h', opts)
+map('n', '<C-j>', '<C-w>j', opts)
+map('n', '<C-k>', '<C-w>k', opts)
+map('n', '<C-l>', '<C-w>l', opts)
