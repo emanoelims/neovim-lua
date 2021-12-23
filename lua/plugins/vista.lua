009d6b717f9ead2ad5e9c74dd4ea4f9173d02753
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-22 16:24:59 -0800

    feat: change some default icons

--- lua/plugins/vista.lua
+++ lua/plugins/vista.lua
@@ -3,3 +3,12 @@ local cmd = vim.cmd
 
 g.vista_default_executive = 'ctags'
 cmd [[let g:vista#renderer#enable_icon = 1]]
+
+cmd [[
+  let g:vista#renderer#icons = {
+  \   "function": "\u0192",
+  \   "variable": "\uf00d",
+  \   "prototype": "\uf013",
+  \   "macro": "\uf00b",
+  \ }
+]]
