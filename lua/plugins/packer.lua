77994ae04a891fd3d15ce5bab57e75709925399a
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-29 16:15:47 -0300

    feat: add plugin signature

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -35,14 +35,16 @@ return require('packer').startup(function()
 
   -- LSP
   use {
-    'hrsh7th/nvim-cmp',
+    'neovim/nvim-lspconfig',
+    'williamboman/nvim-lsp-installer',
 
-    'L3MON4D3/LuaSnip',
+    'hrsh7th/nvim-cmp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lsp',
-    'neovim/nvim-lspconfig',
+    'ray-x/lsp_signature.nvim',
+
+    'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
-    'williamboman/nvim-lsp-installer',
   }
 end)
