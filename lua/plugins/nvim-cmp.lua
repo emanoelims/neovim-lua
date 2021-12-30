f8220b05e2ea214d12fdcab605e5808ba5ab74fc
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-30 09:56:58 -0300

    feat: add plugin lspkind

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,5 +1,7 @@
-local luasnip = require 'luasnip'
 local cmp = require'cmp'
+local lspkind = require 'lspkind'
+local luasnip = require 'luasnip'
+
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -11,6 +13,12 @@ cmp.setup{
   },
   experimental = {
     ghost_text = true,
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      with_text = false,
+      maxwidth = 50
+    })
   },
   snippet = {
     expand = function(args)
