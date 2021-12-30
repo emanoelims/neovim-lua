53b15761901e790941424574130d007d7384c160
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-30 10:05:13 -0300

    feat: add ensure installed languages

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,20 @@
 require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
+    use_languagetree = true,
   },
   autotag = {
     enable = true,
+  },
+  ensure_installed = {
+    'lua',
+    'vim',
+    'tsx',
+    'css',
+    'java',
+    'html',
+    'scss',
+    'clojure',
+    'javascript',
   }
 }
