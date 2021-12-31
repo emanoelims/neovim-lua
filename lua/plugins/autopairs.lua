8310c013790d0769904531d6835e4fad31c9f77b
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-31 00:05:09 -0300

    refactor: add autopairs to cmp

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -4,3 +4,11 @@ if not status_ok then
 end
 
 autopairs.setup {}
+
+local cmp_autopairs_ok, cmp_autopairs = pcall(require, 'nvim-autopairs.completion.cmp')
+local cmp_ok, cmp = pcall(require, 'cmp')
+if not cmp_ok or not cmp_autopairs_ok then
+  return
+end
+
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done {map_char = { tex = '' }})
