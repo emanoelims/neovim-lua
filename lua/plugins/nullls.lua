9e942a6abf020dbb2589c6682664dde3ccaff979
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-31 12:27:54 -0300

    refactor: change prettier to fast prettierd

--- lua/plugins/nullls.lua
+++ lua/plugins/nullls.lua
@@ -7,7 +7,7 @@ null_ls.setup {
   sources = {
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd,
   },
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
