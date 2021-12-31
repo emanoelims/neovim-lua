02828aea5ae65800f1f6b47b78388584ad88a47d
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-31 13:25:53 -0300

    refactor: prefer local binaries

--- lua/plugins/nullls.lua
+++ lua/plugins/nullls.lua
@@ -5,9 +5,15 @@ end
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.diagnostics.eslint_d.with {
+      only_local = 'node_modules/.bin',
+    },
+    null_ls.builtins.code_actions.eslint_d.with {
+      only_local = 'node_modules/.bin',
+    },
+    null_ls.builtins.formatting.prettierd.with {
+      only_local = 'node_modules/.bin',
+    },
   },
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
