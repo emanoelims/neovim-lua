0086257f0343d2c3333953fd6ab5ca0aa7a39a35
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-31 09:06:09 -0300

    refactor: move shortcut settings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,3 +8,9 @@ telescope.setup {
     file_ignore_patterns = {'node_modules', 'dist'}
   }
 }
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map("n", "<Leader><Leader>", ":Telescope find_files<CR>", opts)
+map("n", "<C-p>", ":Telescope buffers<CR>", opts)
