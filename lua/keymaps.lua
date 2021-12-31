62fdeb76485e89d54412d6967409402a9956d7e1
Emmanoel Mendes <dev.emmanoel@hotmail.com>
2021-12-31 08:10:19 -0300

    refactor: move shortcut settings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,10 +14,6 @@ map('n', '<C-l>', '<C-w>l', opts)
 
 map('n', '<leader>q', ':qa!<CR>', opts)
 
-map('n', '<C-n>', ':NvimTreeToggle<CR>', opts)
-map('n', '<leader>r', ':NvimTreeRefresh<CR>', opts)
-map('n', '<leader>n', ':NvimTreeFindFile<CR>', opts)
-
 map('n', '<C-m>', ':Vista!!<CR>', opts)
 
 map("n", "<Leader><Leader>", ":Telescope find_files<CR>", opts)
